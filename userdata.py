8f0e2bb598a8e07d0b736f23a96e16f330fe5979
Jamil Ahmed <slinkycity@gmail.com>
2024-07-03 14:53:17 +0500

    Create userdata.py

---
+++ userdata.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# userdata will be the custom module
+import json
+from ansible.module_utils.basic import AnsibleModule
+import sys
+def main():
+    module = AnsibleModule(
+        argument_spec = dict(
+            name = dict(required=True, type='str'),
+            age  = dict(required=True, type='str'),
+        )
+    )
+    name = module.params['name']
+    age = module.params['age']
+    data = dict(
+        output="your data has stored successfully :-) ",
+    )
+    try:
+        file = open("/tmp/userdata.txt","r")
+        file.write(name+ "," + age + "\n")
+        module.exit_json(changed=True, success=data,msg=data)
+    except Exception as e:
+        module.fail_json(msg='something went wrong. :-(')
+if __name__ == '__main__':
+    main()
